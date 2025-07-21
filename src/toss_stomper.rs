0f479bda0170d3cc050612a521b30d1967ec3f8d
JinsBurger <glare470@gmail.com>
2025-07-22 00:10:15 +0900

    Add subscribe stomper

--- src/toss_stomper.rs
+++ src/toss_stomper.rs
@@ -1,7 +1,11 @@
+use std::{collections::HashMap, hash::Hash};
+
 pub struct TossStomper {
     conn_id: String,
     dev_id: String,
     utk_id: String,
+    stock_id_map: HashMap<String, u32>,
+    cur_stk_id: u32
 }
 
 
@@ -10,7 +14,9 @@ impl TossStomper {
         Self {
             conn_id: conn_id,
             dev_id: dev_id,
-            utk_id: utk_id
+            utk_id: utk_id,
+            stock_id_map: HashMap::new(),
+            cur_stk_id: 0
         }
     }
 
@@ -19,5 +25,10 @@ impl TossStomper {
             "CONNECT\ndevice-id:{}\nconnection-id:{}\nauthorization:{}\naccept-version:1.2,1.1,1.0\nheart-beat:5000,5000\n\n\x00\n",
             self.dev_id, self.conn_id, self.utk_id
         ).into_bytes()
+    }
+
+    pub fn subscribe(&mut self, stock_code: String) -> Vec<u8> {
+        let dest = format!("/topic/quote/stock/{}", stock_code);
+
     }
 }
